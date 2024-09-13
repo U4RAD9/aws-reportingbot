7e83c2a1816ed0ee8b8da1ef7271ab8073b55989
Aman1229 <amanguptadbg1229@gmail.com>
2024-09-13 13:04:19 +0530

    ecg updated

--- authsysproject/JS/src/index.js
+++ authsysproject/JS/src/index.js
@@ -497,29 +497,56 @@ class App extends Component {
     return filename;
   }
 
-  getDataUri(url) {
-    return new Promise((resolve) => {
-      var image = new Image();
-      //image.setAttribute("crossOrigin", "anonymous"); //getting images from external domain
-      image.crossOrigin = "anonymous"; // This should be used to fetch images from external domains
-      image.onload = function () {
-        var canvas = document.createElement("canvas");
-        canvas.width = this.naturalWidth;
-        canvas.height = this.naturalHeight;
+  // getDataUri(url) {
+  //   return new Promise((resolve) => {
+  //     var image = new Image();
+  //     //image.setAttribute("crossOrigin", "anonymous"); //getting images from external domain
+  //     image.crossOrigin = "anonymous"; // This should be used to fetch images from external domains
+  //     image.onload = function () {
+  //       var canvas = document.createElement("canvas");
+  //       canvas.width = this.naturalWidth;
+  //       canvas.height = this.naturalHeight;
 
-        //next three lines for white background in case png has a transparent background
-        var ctx = canvas.getContext("2d");
-        ctx.fillStyle = "#fff"; /// set white fill style
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //       //next three lines for white background in case png has a transparent background
+  //       var ctx = canvas.getContext("2d");
+  //       ctx.fillStyle = "#fff"; /// set white fill style
+  //       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        canvas.getContext("2d").drawImage(this, 0, 0);
+  //       canvas.getContext("2d").drawImage(this, 0, 0);
 
-        resolve(canvas.toDataURL("image/jpeg"));
-      };
+  //       resolve(canvas.toDataURL("image/jpeg"));
+  //     };
 
-      image.src = url;
+  //     image.src = url;
+  //   });
+  // }
+
+  getDataUri(e) {
+    return new Promise((resolve, reject) => {
+        var img = new Image();
+        img.setAttribute("crossOrigin", "anonymous");
+
+        img.onload = function() {
+            var canvas = document.createElement("canvas");
+            canvas.width = this.naturalWidth;
+            canvas.height = this.naturalHeight;
+
+            var ctx = canvas.getContext("2d");
+            ctx.fillStyle = "#fff"; // Fill background with white
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.drawImage(this, 0, 0); // Draw the image
+            resolve(canvas.toDataURL("image/jpeg")); // Return data URI
+        };
+
+        img.onerror = function() {
+            reject(new Error("Failed to load image: " + e));
+        };
+
+        img.src = e; // Set image source
     });
   }
+
 
   ///////////////////////////////// PDF GENERATION CODE /////////////////////////////////////////
   GetDivContentAsJSON() {
