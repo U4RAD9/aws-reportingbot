2c70ef5c3088daccd7f7520641b88e6b2c197bca
Aman1229 <amanguptadbg1229@gmail.com>
2024-11-26 20:39:32 +0530

    data

--- authsysproject/users/models/patientdata.py
+++ authsysproject/users/models/patientdata.py
@@ -13,14 +13,7 @@ class PatientInfo(models.Model):
     NearVisionRight = models.CharField(max_length=250, null=True, default=None, blank=True)
     NearVisionLeft = models.CharField(max_length=250, null=True, default=None, blank=True)
     ColorBlindness = models.CharField(max_length=250, null=True, default=None, blank=True)
-    SphericalRight = models.CharField(max_length=250, null=True, default=None, blank=True)
-    CylindricalRight = models.CharField(max_length=250, null=True, default=None, blank=True)
-    AxisRight = models.CharField(max_length=250, null=True, default=None, blank=True)
-    AddRight = models.CharField(max_length=250, null=True, default=None, blank=True)
-    SphericalLeft = models.CharField(max_length=250, null=True, default=None, blank=True)
-    CylindricalLeft = models.CharField(max_length=250, null=True, default=None, blank=True)
-    AxisLeft = models.CharField(max_length=250, null=True, default=None, blank=True)
-    AddLeft = models.CharField(max_length=250, null=True, default=None, blank=True)
+    
 
     def __str__(self):
         return self.PatientName
