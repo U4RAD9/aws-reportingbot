636f7669a7dbdce6b31ecad84582e0ac9ab76f81
Aman1229 <amanguptadbg1229@gmail.com>
2025-04-16 16:39:08 +0530

    logo abnd excel changes

--- authsysproject/users/views.py
+++ authsysproject/users/views.py
@@ -3458,7 +3458,7 @@ def add_logo_to_pdf(request, pdf_id):
             # logo_path = 'static/logo.png'  # should be accessible locally or from static folder
             logo_path = os.path.join(settings.BASE_DIR, 'users', 'static', 'company_logos', 'logo.png')
             #c.drawImage(logo_path, x=450, y=750, width=100, height=50, mask='auto')  # position logo
-            c.drawImage(logo_path, x=40, y=750, width=120, height=60, mask='auto')
+            c.drawImage(logo_path, x=30, y=770, width=160, height=80, mask='auto')
             c.save()
             overlay_pdf_path = overlay_temp.name
 
