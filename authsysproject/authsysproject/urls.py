92bc45b485d9440d8905091272141e584c63c5a9
Aman1229 <amanguptadbg1229@gmail.com>
2025-09-04 12:40:12 +0530

    study uid field added

--- authsysproject/authsysproject/urls.py
+++ authsysproject/authsysproject/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Add Debug Toolbar URLs only in DEBUG mode
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ]
