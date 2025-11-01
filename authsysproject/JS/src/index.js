fce863c69ae5911bed8f68e611615278e90725ce
rjangid631 <rohanjangid631@gmail.com>
2025-11-01 15:51:51 +0530

    for victoria logo 4

--- authsysproject/JS/src/index.js
+++ authsysproject/JS/src/index.js
@@ -3409,8 +3409,8 @@ UploadDivContentOnPDFWithoutImage() {
         compress: true,
       });
 
-      const topMargin = isVictoriaClinic ? 105 : 200; // Reduced spacing for Victoria clinics
-      const demographicsStartY = isVictoriaClinic ? 85 : 180;
+      const topMargin = isVictoriaClinic ? 95 : 200; // Reduced spacing for Victoria clinics
+      const demographicsStartY = isVictoriaClinic ? 75 : 180;
       const bottomMargin = 90;
       const pageHeight = pdf.internal.pageSize.height;
       let currentYPosition = topMargin;
